1e35cd4e61a7fb4fe97cb7d36d7cd1e650d4816f
young0915 <38437784+young0915@users.noreply.github.com>
2024-10-09 22:13:29 +0900

    자료구조

---
+++ DataStuct/PriorityQueue.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "Arrary.h"
+#include "Vector.h"
+#include "Board.h"
+#include "Player.h"
+using namespace std;
+
+Board board;
+Player player;
+
+int main()
+{
+
+	::srand(static_cast<unsigned>(time(nullptr)));
+
+	board.Init(25, &player);
+	player.Init(&board);
+
+	uint64 lastTick = 0;
+
+	while (true)
+	{
+#pragma region 프레임 관리
+		const uint64 currentTick = ::GetTickCount64();
+		const uint64 deltaTick = currentTick - lastTick;
+		lastTick = currentTick;
+#pragma endregion
+
+		// 입력
+
+		// 로직
+		player.Update(deltaTick);
+
+		// 렌더링
+		board.Render();
+	}
+}
