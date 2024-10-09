d63f0abdf3cb8025522897b7abc94d3000adfd5e
young0915 <38437784+young0915@users.noreply.github.com>
2024-10-09 22:15:59 +0900

    Update Tree.cpp

--- DataStuct/Tree.cpp
+++ DataStuct/Tree.cpp
@@ -1,38 +1,120 @@
-﻿#include "pch.h"
 #include <iostream>
-#include "Arrary.h"
-#include "Vector.h"
-#include "Board.h"
-#include "Player.h"
 using namespace std;
+#include <vector>
 
-Board board;
-Player player;
+class Node
+{
+public :
+   Node(const char* data) : data(data) {}
+public : 
+   const char* data;
+   vector<Node*> children;
+};
+
+Node* CreateTree()
+{
+   Node* root = new Node("R1 개발실");
+   // 기획
+   {
+      Node* node = new Node("디자인 팀");
+      root->children.push_back(node);
+      {
+         Node* leaf = new Node("전투");
+         node->children.push_back(leaf);
+      }
+      {
+         Node* leaf = new Node("경제");
+         node->children.push_back(leaf);
+      }
+      {
+         Node* leaf = new Node("스토리");
+         node->children.push_back(leaf);
+      }
+
+   }
+   // 프로그래밍 팀
+   {
+      Node* node = new Node("프로그래밍 팀");
+      root->children.push_back(node);
+      {
+         Node* leaf = new Node("클라");
+         node->children.push_back(leaf);
+      }
+      {
+         Node* leaf = new Node("서버");
+         node->children.push_back(leaf);
+      }
+      {
+         Node* leaf = new Node("엔진");
+         node->children.push_back(leaf);
+      }
+
+   }
+   // 아트 팀
+   {
+      Node* node = new Node("아트팀");
+      root->children.push_back(node);
+      {
+         Node* leaf = new Node("배경");
+         node->children.push_back(leaf);
+      }
+      {
+         Node* leaf = new Node("캐릭터");
+         node->children.push_back(leaf);
+      }
+
+   }
+   return root;
+}
+
+
+// 깊이(depth) : 루트에서 어떤 노드에 도달하기 위해 거쳐야하는 간선의 개수
+void PrintTree(Node* root, int depth =0)
+{
+   for (int i = 0; i < depth; i++)
+   {
+      cout << "-";
+   }
+
+   cout << root->data << endl;
+
+   int size = root->children.size();
+
+   if (size == 0)
+      return;
+
+   for (int i = 0; i < size; i++)
+   {
+      Node* node = root->children[i];
+      PrintTree(node,depth +1);
+   }
+}
+
+// 트리 높이 
+int GetHeight(Node* root)
+{
+   int ret = 1;
+
+   int size = root->children.size();
+
+   for (int i = 0; i < size; i++)
+   {
+      Node* node = root->children[i];
+      int h = GetHeight(node) + 1;
+
+      if (h > ret)
+      {
+         ret = h;
+      }
+   }
+   return ret;
+}
 
 int main()
 {
+   Node* root = CreateTree();
+   PrintTree(root);
+   cout << endl;
 
-	::srand(static_cast<unsigned>(time(nullptr)));
-
-	board.Init(25, &player);
-	player.Init(&board);
-
-	uint64 lastTick = 0;
-
-	while (true)
-	{
-#pragma region 프레임 관리
-		const uint64 currentTick = ::GetTickCount64();
-		const uint64 deltaTick = currentTick - lastTick;
-		lastTick = currentTick;
-#pragma endregion
-
-		// 입력
-
-		// 로직
-		player.Update(deltaTick);
-
-		// 렌더링
-		board.Render();
-	}
+   cout << GetHeight(root);
 }
